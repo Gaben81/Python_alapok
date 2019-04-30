43e5ec8db8a99c94c1c6c129c1a017da880bcff4
Gaben81 <gabor.trinka@gmail.com>
2019-04-30 23:14:13 +0200

    Initial Commit

---
+++ main_full.py
@@ -0,0 +1,22 @@
+#sum=0
+#for i in range (1, 102): 
+ #   if i % 2 == 0:
+ #       sum += i
+#print (sum)
+
+
+# 50 függvényhívás, függvény, változó, ciklus, elágazás
+
+# megismerkedés :)
+#név = input ('Hogy hívnak téged?')
+#print('Szervusz', név, 'örülök, hogy találkoztunk.',  sep='')
+
+
+#vezeteknev = input ('Mi a vezetékvneved? ')
+#keresztnev = input ('Mi a keresztneved?')
+#print('Szia',vezeteknev, keresztnev,',','örülök, hogy találkoztunk :)!')
+
+vezeteknev = input('Mi a vezetekneved?')
+keresztnev = input('Mi a keresztneved')
+kor = input('Hany eves vagy?')
+print('Szia', keresztnev, 'tényleg', kor, 'eves vagy? Nagyon jol tartod magad :)!!')
